25b5e9323444a300bacc7f4b341a7b4f993e8ebd
Chris DeBoever <cdeboever3@gmail.com>
2015-07-14 13:31:18 -0700

    Notebook updates
    I'm in the process of updating the notebooks. I'll likely do one more
    commit before purging old stuff from them.

--- ciepy/ciepy/__init__.py
+++ ciepy/ciepy/__init__.py
@@ -1,3 +1,4 @@
+import gzip 
 import os
 from os.path import join
 import subprocess
@@ -24,3 +25,11 @@ def makedir(p):
 def submit_job(fn):
     subprocess.check_call('ssh cdeboever@flc.ucsd.edu \'qsub {}\''.format(fn),
                           shell=True)
+        
+def read_emmax_output(fn):
+    with gzip.open(fn) as f:
+        lines = [x.strip().split('\t') for x in f.readlines()]
+    lines[0][0] = lines[0][0][1:]
+    res = pd.DataFrame(lines[1:], columns=lines[0])
+    res = res.convert_objects(convert_numeric=True)
+    return res
