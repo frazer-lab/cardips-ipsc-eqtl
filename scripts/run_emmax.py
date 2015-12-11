8dad015ecab81257215d7981621c559cc85c0993
Christopher DeBoever <cdeboever3@gmail.com>
2015-12-11 15:59:01 -0800

    Updates for running eQTL analysis
    I've updated the run_emmax.py script. I still need to update the
    notebook but I want to commit the current state.

--- scripts/run_emmax.py
+++ scripts/run_emmax.py
@@ -1,13 +1,38 @@
 import argparse
 import gzip
 import os
+import random
+random.seed(20150605)
 import shutil
 import subprocess
 import sys
 
 import pandas as pd
 
-def _ind(indf, ind):
+def _cov(covf, covariates, samples):
+    """
+    Make .cov file for EMMAX. The first column is individual ID and the
+    subsequent files are covariates.
+    
+    Parameters
+    ----------
+    covf : str
+        Path for output cov file.
+
+    covariates : str
+        Path to input covariates file. This file will be checked and written in
+        the correct order for EMMAX.
+
+    samples : str
+        Pandas Series with samples (in correctly sorted order) as values.
+
+    """
+    c = pd.read_table(covariates, header=None, index_col=0)
+    assert len(set(c.index) & set(samples.values)) == samples.shape[0], \
+            'Covariates and samples files do not match.'
+    c.ix[samples.values].to_csv(covf, sep='\t', header=False)
+
+def _ind(indf, samples):
     """
     Make .ind file for EMMAX. The first column is individual ID and the second
     column is the position of the sample in VCF file. Since I make the VCF based
@@ -16,11 +41,11 @@ def _ind(indf, ind):
     
     Parameters
     ----------
-    inf : str
+    indf : str
         Path for output ind file.
 
-    ind : str
-        Input ind file.
+    samples : str
+        Pandas Series with samples (in correctly sorted order) as values.
 
     phenos : pandas.DataFrame
         DataFrame with phenotype values.
@@ -29,11 +54,10 @@ def _ind(indf, ind):
         A list of indices to permute the phenotype data before writing it.
 
     """
-    se = pd.read_table(ind, index_col=0, squeeze=True, header=None)
-    se = pd.Series(range(1, se.shape[0] + 1), index=se.index)
+    se = pd.Series(range(1, samples.shape[0] + 1), index=samples.values)
     se.to_csv(indf, header=False, sep='\t')
 
-def _phe(phenof, gene_id, phenos, permut=None):
+def _phe(phenof, phenos, permut=False):
     """
     Make .phe file for EMMAX. The first column is individual ID and the second
     column is the phenotype value.
@@ -43,22 +67,22 @@ def _phe(phenof, gene_id, phenos, permut=None):
     phenof : str
         Path for output phe file.
 
-    gene_id : str
-        ID for gene (must be contained in phenos.index).
+    phenos : pandas.Series
+        Pandas Series with phenotype as values and correctly sorted sample IDs
+        as index.
 
-    phenos : pandas.DataFrame
-        DataFrame with phenotype values.
-
-    permut : list
-        A list of indices to permute the phenotype data before writing it.
+    permut : bool
+        If true, permute the expression values.
 
     """
-    se = phenos.ix[gene_id]
     if permut:
-        se = pd.Series(se.values[permut], index=se.index)
-    se.to_csv(phenof, header=False, sep='\t')
+        p = random.sample(range(phenos.shape[0]), phenos.shape[0])
+        pd.Series(phenos.values[p], index=phenos.index).to_csv(
+            phenof, header=False, sep='\t')
+    else:
+        phenos.to_csv(phenof, header=False, sep='\t')
 
-def _reml(eigf, remlf, phe, ind, kin, pEmmax_path, cov=None):
+def _reml(eigf, remlf, phef, indf, kinf, pEmmax_path, covf=None):
     """
     Make reml file for EMMAX.
     
@@ -74,13 +98,13 @@ def _reml(eigf, remlf, phe, ind, kin, pEmmax_path, cov=None):
     c = ('{} reml --phenof {} --kinf {} --indf {} --out-eigf {} '
          '--out-remlf {}'.format(
              pEmmax_path,
-             phe,
-             kin,
-             ind,
+             phef,
+             kinf,
+             indf,
              eigf,
              remlf))
-    if cov:
-        c += ' --covf {}'.format(cov)
+    if covf:
+        c += ' --covf {}'.format(covf)
     subprocess.check_call(c, shell=True)
 
 def run_emmax(
@@ -88,14 +112,15 @@ def run_emmax(
     vcf, 
     regions, 
     phenotypes, 
-    ind, 
+    samples, 
     kin, 
     tempdir, 
     outdir,
-    cov=None, 
+    covariates=None, 
     num_lesser_pval=15, 
     min_permut=1000, 
     max_permut=10000,
+    maf=0.05,
     bcftools_path='bcftools',
     pEmmax_path='pEmmax',
     verbose=False,
@@ -119,9 +144,8 @@ def run_emmax(
         IDs that are in the same order as the ind and cov files. Index should be
         phenotype names (i.e. gene IDs).
     
-    ind : str
-        Path to ind file for EMMAX. Order should match order of cov phenotypes
-        files.
+    samples : str
+        Path to file defining samples (one per line) to use for EMMAX. 
         
     kin : str
         Path to kinship matrix file.
@@ -132,8 +156,12 @@ def run_emmax(
     outdir : str
         Path to directory where results will be saved.
         
-    cov : str 
-        Path to covariates file for EMMAX.
+    covariates : str 
+        Covariates file for use with EMMAX. This should be a tab separated file
+        with NO header. The first column is sample names. Subsequent columns are
+        covariates. Categorical covariates should be binarized.  I think all
+        covariates should be numerical (so male-female should be coded as 1-2
+        etc.).
         
     num_lesser_pval : int
         Number of p-values less than minimum gene p-value that must be observed
@@ -145,9 +173,11 @@ def run_emmax(
     max_permut : int
         The maximum number of permutations to test.
 
+    maf : float
+        Minor allele frequency cut-off. Variants below this frequency will not
+        be tested for association.
+
     """
-    import random
-    random.seed(20150605)
     if verbose:
         import datetime
 
@@ -159,10 +189,16 @@ def run_emmax(
 
     curdir = os.path.realpath(os.curdir)
     os.chdir(tempdir)
+    
+    # I believe the EMMAX input files should all be sorted by sample in the same
+    # order, so I'll define a sample ordering here and use it throughout.
+    sorted_samples = pd.read_table(samples, header=None, squeeze=True)
+    sorted_samples.sort_values(inplace=True)
 
     # Make VCF file. This VCF file will only have biallelic variants in the
     # regions of interest.
-    vcf = _make_emmax_vcf(vcf, gene_id, tempdir, regions, ind, bcftools_path)
+    vcf = _make_emmax_vcf(vcf, gene_id, tempdir, regions, sorted_samples, maf,
+                          bcftools_path)
     if verbose:
         res = str(datetime.datetime.now())
         sys.stdout.write('VCF file created at {}.\n'.format(res))
@@ -170,17 +206,25 @@ def run_emmax(
 
     # Make ind file.
     indf = os.path.join(tempdir, '{}.ind'.format(gene_id))
-    _ind(indf, ind)
+    _ind(indf, sorted_samples)
     if verbose:
         res = str(datetime.datetime.now())
         sys.stdout.write('ind file created at {}.\n'.format(res))
         sys.stdout.flush()
 
+    # Make cov file.
+    covf = os.path.join(tempdir, '{}.cov'.format(gene_id))
+    _cov(covf, covariates, sorted_samples)
+    if verbose:
+        res = str(datetime.datetime.now())
+        sys.stdout.write('cov file created at {}.\n'.format(res))
+        sys.stdout.flush()
+
     # Make phe file.
-    order = pd.read_table(ind, index_col=0, header=None)
-    phenos = pd.read_table(phenotypes, index_col=0)[order.index]
+    phenos = pd.read_table(phenotypes, index_col=0)
+    phenos = phenos.ix[gene_id, sorted_samples.values]
     phenof = os.path.join(tempdir, '{}.phe'.format(gene_id))
-    _phe(phenof, gene_id, phenos)
+    _phe(phenof, phenos, permut=False)
     if verbose:
         res = str(datetime.datetime.now())
         sys.stdout.write('phe file created at {}.\n'.format(res))
@@ -189,21 +233,21 @@ def run_emmax(
     # Make reml file.
     eigf = os.path.join(tempdir, '{}.eigR'.format(gene_id))
     remlf = os.path.join(outdir, '{}.reml'.format(gene_id))
-    _reml(eigf, remlf, phenof, indf, kin, pEmmax_path, cov=cov)
+    _reml(eigf, remlf, phenof, indf, kin, pEmmax_path, covf=covf)
     if verbose:
         res = str(datetime.datetime.now())
         sys.stdout.write('reml file created at {}.\n'.format(res))
         sys.stdout.flush()
 
     # Run association.
+    real_res = _emmax(vcf, phenof, indf, eigf, remlf, maf, pEmmax_path)
     out = os.path.join(outdir, '{}.tsv'.format(gene_id))
-    _emmax(out, vcf, phenof, indf, eigf, remlf, pEmmax_path)
+    real_res.to_csv(out, sep='\t', index=False, na_rep='NA')
     _emmax_cleanup(gene_id)
     if verbose:
         res = str(datetime.datetime.now())
         sys.stdout.write('First association completed at {}.\n'.format(res))
         sys.stdout.flush()
-    real_res = pd.read_table(out)
     min_pval = real_res.PVALUE.min()
 
     # Do the previous steps for permutations.
@@ -218,21 +262,19 @@ def run_emmax(
         sys.stdout.flush()
     while i < max_permut:
         # Run EMMAX for this permutation.
-        p = random.sample(range(phenos.shape[1]), phenos.shape[1])
         phenof = os.path.join(tempdir, '{}.phe'.format(gene_id))
-        _phe(phenof, gene_id, phenos, permut=p)
+        _phe(phenof, phenos, permut=True)
         eigf = os.path.join(tempdir, '{}.eigR'.format(gene_id))
         remlf = os.path.join(tempdir, '{}.reml'.format(gene_id))
-        _reml(eigf, remlf, phenof, indf, kin, pEmmax_path, cov=cov)
+        _reml(eigf, remlf, phenof, indf, kin, pEmmax_path, covf=covf)
         out = os.path.join(tempdir, '{}_permutation_{}.tsv'.format(
             gene_id, str(i + 1).zfill(len(str(max_permut)))))
-        _emmax(out, vcf, phenof, indf, eigf, remlf, pEmmax_path)
+        res = _emmax(vcf, phenof, indf, eigf, remlf, maf, pEmmax_path)
 
         # Read results.
-        res = pd.read_table(out)
         res.index = ('chr' + res['#CHROM'].astype(str) + ':' + 
                      res.BEG.astype(str))
-        pvalues.append(res.PVALUE)
+        pvalues.append(res.PVALUE.values)
         m = res.PVALUE.min()
         min_pvalues.append(m)
         reml_info.append(pd.read_table(remlf, header=None, index_col=0,
@@ -242,7 +284,6 @@ def run_emmax(
         if (num_lesser_pvals >= num_lesser_pval) and (i + 1 >= min_permut):
             break
         i += 1
-        os.remove(out)
         
         _emmax_cleanup(gene_id)
         if verbose:
@@ -255,7 +296,7 @@ def run_emmax(
     os.remove('{}.vcf.gz.tbi'.format(gene_id))
     if verbose:
         res = str(datetime.datetime.now())
-        sys.stdout.write('VCF file removed at {}.\n'.format(i, res))
+        sys.stdout.write('VCF file removed at {}.\n'.format(res))
         sys.stdout.flush()
    
     pvalues = pd.DataFrame(pvalues).T
@@ -271,24 +312,21 @@ def run_emmax(
     shutil.rmtree(tempdir)
     if verbose:
         res = str(datetime.datetime.now())
-        sys.stdout.write('Finished at {}.\n'.format(i, res))
+        sys.stdout.write('Finished everything at {}.\n'.format(res))
         sys.stdout.flush()
 
-def _emmax(out, vcf, phe, ind, eig, reml, pEmmax_path):
+def _emmax(vcf, phe, ind, eig, reml, maf, pEmmax_path):
     """
     Execute EMMAX command.
 
     Parameters
     ----------
-    out : str
-        Output file?
-    
     vcf : str
         Path to VCF file that contains only the variants to test.
         
     """
     c = ('{} assoc --vcf {} --phenof {} --field GT --indf {} --eigf {} '
-         '--remlf {} --out-assocf {} --minMAF 0.1 --maxMAF 1 --maxMAC '
+         '--remlf {} --out-assocf /dev/stdout --minMAF {} --maxMAF 1 --maxMAC '
          '1000000000 --minRSQ 0 --minCallRate 0.5 --minMAC 3'.format(
              pEmmax_path,
              vcf,
@@ -296,9 +334,14 @@ def _emmax(out, vcf, phe, ind, eig, reml, pEmmax_path):
              ind,
              eig,
              reml,
-             out
+             maf,
          ))
-    subprocess.check_call(c, shell=True)
+    res = subprocess.check_output(c, shell=True)
+    lines = res.split('\n')[0:-1]
+    lines = [x.split('\t') for x in lines]
+    df = pd.DataFrame(lines[1:], columns=lines[0])
+    df['PVALUE'] = pd.to_numeric(df['PVALUE'], errors='coerce')
+    return df
 
 def _emmax_cleanup(prefix):
     """
@@ -306,21 +349,52 @@ def _emmax_cleanup(prefix):
     """
     to_delete = ['eigR', 'phe', 'reml']
     for suffix in to_delete:
-        fn = '{}.{}'.format(prefix, suffix)
-        if os.path.exists(fn):
-            os.remove(fn)
+        try:
+            os.remove('{}.{}'.format(prefix, suffix))
+        except OSError:
+            continue
 
-def _make_emmax_vcf(vcf, gene_id, tempdir, regions, ind, bcftools_path):
-    import ciepy
+def _make_emmax_vcf(
+    vcf, 
+    gene_id, 
+    tempdir, 
+    regions, 
+    samples, 
+    maf,
+    bcftools_path,
+):
+    """
+    Make VCF file for EMMAX. Rather than reading into the larger VCF file, we'll
+    make a small VCF with just the samples and variants to test for this gene.
+    
+    Parameters
+    ----------
+    vcf : str
+        Path to VCF file with all samples and variants. Must be compressed and
+        indexed.
 
-    ind = pd.read_table(ind, index_col=0, header=None)
+    gene_id : str
+        Gene ID used for naming files.
+
+    regions : list
+        List of regions to get variants from.
+
+    samples : str
+        Pandas Series with samples (in correctly sorted order) as values.
+
+    maf : float
+        Minor allele frequency cut off. Any variants with MAF lower than this
+        will be filtered out.
+
+    """
     fn = os.path.join(tempdir, '{}.vcf.gz'.format(gene_id))
-    c = ('{} view {} -q 0.05:minor -m2 -M2 -r {} -s {} -O u | '
+    c = ('{} view {} -q {}:minor -m2 -M2 -r {} -s {} -O u | '
          '{} filter -m x -O z > {}'.format(
              bcftools_path,
              vcf,
+             maf,
              ','.join(regions),
-             ','.join(ind.index),
+             ','.join(samples.values),
              bcftools_path,
              fn))
     subprocess.check_call(c, shell=True)
@@ -335,6 +409,7 @@ def main():
     num_lesser_pval = 15
     min_permut = 1000
     max_permut = 10000
+    maf = 0.05
     tempdir = '/dev/shm'
     bcftools_path = 'bcftools'
     pEmmax_path = 'pEmmax'
@@ -345,21 +420,29 @@ def main():
     parser.add_argument('gene_id', help=('Gene ID for gene to test. This '
                                          'should be a column in the phenotypes '
                                          'file.'))
-    parser.add_argument('vcf', help=('VCF file with variants.'))
+    parser.add_argument('vcf', help=('Compressed, indexed VCF file with '
+                                     'all variants.'))
     parser.add_argument('regions', help=(
-        'List of regions of the form chr3:100-200. Multiple regions are '
+        'List of regions of the form chr3:100-200 or (3:100-200 depending on '
+        'your VCF. Multiple regions are '
         'separated by commas: chr3:100-200,chr10:400-500. Biallelic variants '
         'in these regions will be tested.'))
     parser.add_argument('phenotypes', 
-                        help=('TSV file with gene expression values. Index '
+                        help=('TSV file with gene expression (or other) '
+                              'values. Index '
                               'should be gene IDs and columns should be '
-                              'sample IDs that match the ind file.'))
-    parser.add_argument('ind', help=('ind file with positions of each sample '
-                                     'in the VCF header.'))
+                              'sample IDs that match the VCF file.'))
+    parser.add_argument('samples', help=(
+        'File with samples (one per line) to use from VCF file. The sample IDs '
+        'should match those in the VCF file.'))
     parser.add_argument('kin', help=('Kinship matrix file (.kinf).'))
     parser.add_argument('outdir', help=('Directory to store final results.'))
-    parser.add_argument('-c', metavar='cov', 
-                        help=('Covariates file for EMMAX.'))
+    parser.add_argument('-c', metavar='covariates', help=(
+        'Covariates file for use with EMMAX. This should be a tab separated '
+        'file with NO header. The first column is sample names. Subsequent '
+        'columns are covariates. Categorical covariates should be binarized. '
+        'I think all covariates should be numerical (so male-female should be '
+        'coded as 1-2 etc.).'))
     parser.add_argument('-n', metavar='num_lesser_pval',
                         default=num_lesser_pval, type=int, help=(
                             'Minimum number of gene-level permutation p-values '
@@ -374,6 +457,11 @@ def main():
                         default=max_permut, type=int, help=(
                             'Maximum number of permutations to perform. '
                             'Default: {:,}.'.format(max_permut)))
+    parser.add_argument('-m', metavar='maf',
+                        default=maf, type=float, help=(
+                            'Minor allele frequency cut-off. Variants below '
+                            'this frequency will not be tested. Default: '
+                            '{}.'.format(maf)))
     parser.add_argument('-t', metavar='tempdir', help=(
         'Temporary directory. Default: {}.'.format(tempdir)),
                         default=tempdir)
@@ -390,13 +478,14 @@ def main():
     vcf = args.vcf
     regions = args.regions.split(',')
     phenotypes = args.phenotypes
-    ind = args.ind
+    samples = args.samples
     kin = args.kin
     outdir = args.outdir
-    cov = args.c
+    covariates = args.c
     num_lesser_pval = args.n
     min_permut = args.i
     max_permut = args.a
+    maf = args.m
     tempdir = args.t
     bcftools_path = args.b
     pEmmax_path = args.e
@@ -407,14 +496,15 @@ def main():
         vcf, 
         regions, 
         phenotypes, 
-        ind, 
+        samples, 
         kin, 
         tempdir, 
         outdir,
-        cov=cov, 
+        covariates=covariates,
         num_lesser_pval=num_lesser_pval,
         min_permut=min_permut, 
         max_permut=max_permut,
+        maf=maf,
         bcftools_path=bcftools_path,
         pEmmax_path=pEmmax_path,
         verbose=verbose,
